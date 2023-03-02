984be98a9f94451c01c7d63173613a54ec2490a1
Vandy1100 <nagn_vidy@tk.bbu.edu.kh>
2023-03-02 09:14:32 +0700

    add more text

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Data Analytics Class");
         System.out.println(" Error happen");
+        System.out.println("Hello Kon Papa");
     }
 }
