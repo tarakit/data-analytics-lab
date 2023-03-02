023583ba69ede7ff453cb4c7265e5156578f6ba5
Chhanny-Vichhai <vichhai212803@gmail.com>
2023-03-02 09:47:01 +0700

    hi

--- src/Chhannyvichhai.java
+++ src/Chhannyvichhai.java
@@ -1,2 +1,5 @@
-package PACKAGE_NAME;public class Chhannyvichhai {
+public class Chhannyvichhai {
+    public static void main(String[] args) {
+        System.out.println("Add reang klun");
+    }
 }
